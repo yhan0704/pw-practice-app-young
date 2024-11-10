18577108b8e49c40072f18ca9cb9a393961df458
yhan0704 <hanyc1205@gmail.com>
2024-11-10 08:26:48 -0500

    simple playwright text using getByText

---
+++ tests/example.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('get started link', async ({ page }) => {
+  await page.goto('http://localhost:4200/');
+
+  await page.getByText("Forms").click()
+  await page.getByText("Form Layouts").click()
+
+});
